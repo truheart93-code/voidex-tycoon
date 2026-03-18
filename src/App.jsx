44f0c707a5c0abfec1dfb890a73de397da5642fc
Martin Robertson <truheart93@gmail.com>
2026-03-18 04:09:26 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,8 @@ const AuthenticatedApp = () => {
   // Render the main app
   return (
     <Routes>
-      {/* Add your page Route elements here */}
+      <Route path="/" element={<Navigate to="/Game" replace />} />
+      <Route path="/Game" element={<Game />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
